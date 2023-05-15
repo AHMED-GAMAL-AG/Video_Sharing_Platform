184b66c2490af0b3b61887d0f11e7d612ee57a25
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-05-15 16:10:55 +0300

    06. معالجة مقطع الفيديو

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Video;
+use FFMpeg\Coordinate\Dimension;
+use FFMpeg\Format\Video\X264;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Intervention\Image\ImageManagerStatic as Image;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg as SupportFFMpeg;
 
 class VideoController extends Controller
 {
@@ -47,7 +50,7 @@ class VideoController extends Controller
         $image = Image::make($request->image)->resize(320, 180);
         $path = Storage::put($image_path, $image->stream()); // public folder selected in the .env
 
-        $request->video->storeAs('/', $video_path , 'public'); // store the video in public folder
+        $request->video->storeAs('/', $video_path, 'public'); // store the video in public folder
 
         $video = Video::create([
             'disk' => 'public',
@@ -57,6 +60,52 @@ class VideoController extends Controller
             'user_id' => auth()->user()->id,
         ]);
 
+        $low_BitrateFormat = (new X264('aac', 'libx264'))->setKiloBitrate(500); // 240p
+        $low2_BitrateFormat = (new X264('aac', 'libx264'))->setKiloBitrate(900); // 360p
+        $medium_BitrateFormat = (new X264('aac', 'libx264'))->setKiloBitrate(1500); // 480p
+        $high_BitrateFormat = (new X264('aac', 'libx264'))->setKiloBitrate(3000);   // 720p
+
+        $converted_name_240 = '240-' . $video->video_path;
+        $converted_name_360 = '360p-' . $video->video_path;
+        $converted_name_480 = '480p-' . $video->video_path;
+        $converted_name_720 = '720p-' . $video->video_path;
+
+        SupportFFMpeg::fromDisk($video->disk)
+            ->open($video->video_path)
+
+            ->addFilter(function ($filters) { // 240p
+                $filters->resize(new Dimension(426, 240));
+            })
+            ->export()
+            ->toDisk('public')
+            ->inFormat($low_BitrateFormat)
+            ->save($converted_name_240)
+
+            ->addFilter(function ($filters) { // 360p
+                $filters->resize(new Dimension(640, 360));
+            })
+            ->export()
+            ->toDisk('public')
+            ->inFormat($low2_BitrateFormat)
+            ->save($converted_name_360)
+
+            ->addFilter(function ($filters) { // 480p
+                $filters->resize(new Dimension(854, 480));
+            })
+            ->export()
+            ->toDisk('public')
+            ->inFormat($medium_BitrateFormat)
+            ->save($converted_name_480)
+
+            ->addFilter(function ($filters) { // 720p
+                $filters->resize(new Dimension(1280, 720));
+            })
+            ->export()
+            ->toDisk('public')
+            ->inFormat($high_BitrateFormat)
+            ->save($converted_name_720);
+
+        return redirect()->back()->with('success', __('سيكون مقطع الفيديو متوفر في أقصر وقت عندما ننتهي من معالجته'));
     }
 
     /**
