12980ab5d0c89696f2f7cb4bf4514de448ae40c6
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-05-15 13:46:28 +0300

    02. بناء القالب الأساسي للموقع

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,6 +18,8 @@
 
     {{-- Styles --}}
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
 </head>
 
 <body dir="rtl" style="text-align: right">
@@ -67,96 +69,96 @@
                 </ul>
 
                 {{-- <ul class="navbar-nav mr-auto">
-                        @guest
+                    @guest
+                        <li class="nav-item mt-2">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('تسجيل الدخول') }}</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item mt-2">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('تسجيل الدخول') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('إنشاء حساب') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item mt-2">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('إنشاء حساب') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown justify-content-left mt-2">
-                                <a id="navbarDropdown" class="nav-link" href="#" data-toggle="dropdown">
-                                    <img class="h-8 w-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                </a>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown justify-content-left mt-2">
+                            <a id="navbarDropdown" class="nav-link" href="#" data-toggle="dropdown">
+                                <img class="h-8 w-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                            </a>
 
-                                <div class="dropdown-menu dropdown-menu-left px-2 text-right mt-2">
-                                    <!-- Responsive Settings Options -->
-                                    <div class="pt-4 pb-1 border-t border-gray-200">
-                                        <div class="flex items-center px-4">
-                                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                                <div class="shrink-0 ml-3">
-                                                    <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                                </div>
-                                            @endif
-
-                                            <div class="ml-3">
-                                                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                                                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                            <div class="dropdown-menu dropdown-menu-left px-2 text-right mt-2">
+                                <!-- Responsive Settings Options -->
+                                <div class="pt-4 pb-1 border-t border-gray-200">
+                                    <div class="flex items-center px-4">
+                                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                            <div class="shrink-0 ml-3">
+                                                <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                             </div>
-                                        </div>
+                                        @endif
 
-                                        <div class="mt-3 space-y-1">
-                                            <!-- Account Management -->
-                                            <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                                                {{ __('Profile') }}
+                                        <div class="ml-3">
+                                            <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                                            <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                                        </div>
+                                    </div>
+
+                                    <div class="mt-3 space-y-1">
+                                        <!-- Account Management -->
+                                        <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
+                                            {{ __('Profile') }}
+                                        </x-responsive-nav-link>
+
+                                        @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                                            <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
+                                                {{ __('API Tokens') }}
+                                            </x-responsive-nav-link>
+                                        @endif
+
+                                        <!-- Authentication -->
+                                        <form method="POST" action="{{ route('logout') }}" x-data>
+                                            @csrf
+
+                                            <x-responsive-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                                {{ __('Log Out') }}
+                                            </x-responsive-nav-link>
+                                        </form>
+
+                                        <!-- Team Management -->
+                                        @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                                            <div class="border-t border-gray-200"></div>
+
+                                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                                {{ __('Manage Team') }}
+                                            </div>
+
+                                            <!-- Team Settings -->
+                                            <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
+                                                {{ __('Team Settings') }}
                                             </x-responsive-nav-link>
 
-                                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                                <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
-                                                    {{ __('API Tokens') }}
+                                            @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
+                                                <x-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
+                                                    {{ __('Create New Team') }}
                                                 </x-responsive-nav-link>
-                                            @endif
+                                            @endcan
 
-                                            <!-- Authentication -->
-                                            <form method="POST" action="{{ route('logout') }}" x-data>
-                                                @csrf
-
-                                                <x-responsive-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
-                                                    {{ __('Log Out') }}
-                                                </x-responsive-nav-link>
-                                            </form>
-
-                                            <!-- Team Management -->
-                                            @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                                            <!-- Team Switcher -->
+                                            @if (Auth::user()->allTeams()->count() > 1)
                                                 <div class="border-t border-gray-200"></div>
 
                                                 <div class="block px-4 py-2 text-xs text-gray-400">
-                                                    {{ __('Manage Team') }}
+                                                    {{ __('Switch Teams') }}
                                                 </div>
 
-                                                <!-- Team Settings -->
-                                                <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
-                                                    {{ __('Team Settings') }}
-                                                </x-responsive-nav-link>
-
-                                                @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
-                                                    <x-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
-                                                        {{ __('Create New Team') }}
-                                                    </x-responsive-nav-link>
-                                                @endcan
-
-                                                <!-- Team Switcher -->
-                                                @if (Auth::user()->allTeams()->count() > 1)
-                                                    <div class="border-t border-gray-200"></div>
-
-                                                    <div class="block px-4 py-2 text-xs text-gray-400">
-                                                        {{ __('Switch Teams') }}
-                                                    </div>
-
-                                                    @foreach (Auth::user()->allTeams() as $team)
-                                                        <x-switchable-team :team="$team" component="responsive-nav-link" />
-                                                    @endforeach
-                                                @endif
+                                                @foreach (Auth::user()->allTeams() as $team)
+                                                    <x-switchable-team :team="$team" component="responsive-nav-link" />
+                                                @endforeach
                                             @endif
-                                        </div>
+                                        @endif
                                     </div>
                                 </div>
-                            </li>
-                        @endguest
-                    </ul> --}}
+                            </div>
+                        </li>
+                    @endguest
+                </ul> --}}
 
             </div>
         </nav>
